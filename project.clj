7c87df6ef80481ddfd741be7bde1415a9d0ed451
Hoang Minh Thang <p@banphim.net>
2013-01-24 11:58:28 +0700

    set ver to 0.2.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject web-repl "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
+(defproject cl2-web-repl "0.2.0-SNAPSHOT"
+            :description "Chlorine REPL on the Web."
             :dependencies [[org.clojure/clojure "1.4.0"]
                             [noir "1.3.0-beta3"]
-                            [chlorine "1.6.0"]
-                            [boot-cl2 "1.0.0"]]
+                            [chlorine "1.7.0-SNAPSHOT"]
+                            [core-cl2 "1.0.0-SNAPSHOT"]]
             :main web-repl.server)
 
