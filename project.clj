907afedf707f3ad2dc3fa5bdbb3b6646281dee05
Hoang Minh Thang <p@banphim.net>
2013-03-01 12:13:58 +0700

    use chlorine stable

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Chlorine REPL on the Web."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [lib-noir "0.3.5"]
-                 [chlorine "1.7.0-SNAPSHOT"]
+                 [chlorine "1.7.0"]
                  [core-cl2 "1.0.0-SNAPSHOT"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler web-repl.server/handler
