50a3ca67a5cfdcc00e6812b3e8b4efc0919c3566
Hoang Minh Thang <p@banphim.net>
2013-03-08 12:29:35 +0700

    use clojure-1.5.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cl2-web-repl "0.2.0-SNAPSHOT"
+(defproject cl2-web-repl "0.3.0-SNAPSHOT"
   :description "Chlorine REPL on the Web."
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [lib-noir "0.3.5"]
                  [chlorine "1.7.0"]
                  [core-cl2 "1.0.0-SNAPSHOT"]]
