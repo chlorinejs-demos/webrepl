456aa7638d306a2937af4b97a542cfd211506180
Hoang Minh Thang <p@banphim.net>
2013-03-11 19:34:39 +0700

    update to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cl2-web-repl "0.3.0-SNAPSHOT"
   :description "Chlorine REPL on the Web."
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.3.5"]
-                 [chlorine "1.7.0"]
+                 [chlorine "1.5.0"]
                  [core-cl2 "1.0.0-SNAPSHOT"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler web-repl.server/handler
