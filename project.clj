83596dc0171900b136b4f35180804a393c03628c
Hoang Minh Thang <p@banphim.net>
2013-03-29 02:42:30 +0700

    set version to 0.3.2, update dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cl2-web-repl "0.3.1"
+(defproject cl2-web-repl "0.3.2"
   :description "Chlorine REPL on the Web."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.3.5"]
-                 [chlorine "1.5.2"]
-                 [core-cl2 "0.6.0"]]
+                 [chlorine "1.5.2.1"]
+                 [core-cl2 "0.7.0"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler web-repl.server/handler
          }
