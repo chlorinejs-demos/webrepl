fb26bd4498a02df56d650678148aaf0f4cb5c12b
Hoàng Minh Thắng <p@banphim.net>
2013-06-12 01:52:37 +0700

    bump to v0.3.4

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject cl2-web-repl "0.3.3"
+(defproject cl2-web-repl "0.3.4"
   :description "Chlorine REPL on the Web."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [lib-noir "0.3.5"]
-                 [chlorine "1.5.3.1"]
-                 [core-cl2 "0.7.2"]]
-  :plugins [[lein-ring "0.8.2"]]
+                 [lib-noir "0.6.1"]
+                 [chlorine "1.5.4"]
+                 [core-cl2 "0.7.3"]]
+  :plugins [[lein-ring "0.8.5"]]
   :ring {:handler web-repl.server/handler
          }
   :aot :all
