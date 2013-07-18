cb8c7d9e5488a9e76f064e8e6ddea66c600565a0
Hoàng Minh Thắng <p@banphim.net>
2013-07-18 18:34:19 +0700

    update deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cl2-web-repl "0.3.4"
+(defproject cl2-web-repl "0.3.5"
   :description "Chlorine REPL on the Web."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.6.1"]
-                 [chlorine "1.5.4"]
-                 [core-cl2 "0.7.3"]]
+                 [chlorine "1.6.1"]
+                 [core-cl2 "0.8.0"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler web-repl.server/handler
          }
