8d002ca3357b96aacd655ca9f9c3afa671b11b85
Hoàng Minh Thắng <p@banphim.net>
2013-11-11 01:11:04 +0700

    bump to v0.3.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cl2-web-repl "0.3.5"
+(defproject cl2-web-repl "0.3.6-SNAPSHOT"
   :description "Chlorine REPL on the Web."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.6.1"]
-                 [chlorine "1.6.2"]
-                 [core-cl2 "0.8.0"]]
+                 [chlorine "1.6.4-SNAPSHOT"]
+                 [core-cl2 "0.9.0-SNAPSHOT"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler web-repl.server/handler
          }
